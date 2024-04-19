ad7e9c438d73f9cf0fa6276357b2b50b46e1f79a
Ike-Martha <amagwen36@gmail.com>
2024-04-19 14:13:17 +0100

    Add project files.

---
+++ FinalGpa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gpa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = "Ike Martha";
+        }
+    }
+}
